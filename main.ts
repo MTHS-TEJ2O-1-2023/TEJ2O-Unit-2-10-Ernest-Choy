94c7d557c823a6c7bd01687787d0b7a48c9fa1f1
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-18 17:16:44 -0400

    Typever

--- main.ts
+++ main.ts
@@ -5,44 +5,55 @@
  * This program turns on the neopixels depending on the lightLevel
 */
 
+let lightvalue: number
+lightvalue = input.lightLevel()
 let neoPixelStrip: neopixel.Strip = null
 
+// Clean up
+basic.clearScreen()
 neoPixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neoPixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neoPixelStrip.show()
+basic.showIcon(IconNames.Happy)
 
 // cut all colors
-while (true) {
-  if (input.lightLevel() <= 51)
+basic.forever(function() {
+  if (input.lightLevel() <= 51) {
     neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
     neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
     neoPixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neoPixelStrip.show()
-
-  if (input.lightLevel() >= 52)
+  }
+  if (input.lightLevel() >= 52) {
     neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
     neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
     neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
     neoPixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neoPixelStrip.show()
-
-  if (input.lightLevel() > 104)
+  }
+  if (input.lightLevel() > 104) {
     neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
     neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
     neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
     neoPixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neoPixelStrip.show()
-
-  if (input.lightLevel() > 156)
+  }
+  if (input.lightLevel() > 156) {
     neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
     neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
     neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
     neoPixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     neoPixelStrip.show()
-
-  if (input.lightLevel() > 208)
+  }
+  if (input.lightLevel() > 208) {
     neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
     neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
     neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
     neoPixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
     neoPixelStrip.show()
-}
+  }
+})
