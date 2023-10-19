e5f62f51894c9cffd58c1e8a595f08bd72c7bd4f
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-19 17:25:38 +0000

    fix at home

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,62 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Ernest
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import neopixel
 
+neoPixelStrip = None
 
-display.scroll("Hello, World!")
+display.clear
+display.show(Image.HAPPY)
+
+np = neopixel.NeoPixel(pin16, 4)
+np[0] = (0, 0, 0)
+np[1] = (0, 0, 0)
+np[2] = (0, 0, 0)
+np[3] = (0, 0, 0)
+np.show()
+
+while True:
+    light_level = display.read_light_level()
+
+    # if light level is <= 51
+    if light_level <= 51:
+        np[0] = (0, 0, 0)
+        np[1] = (0, 0, 0)
+        np[2] = (0, 0, 0)
+        np[3] = (0, 0, 0)
+        np.show()
+
+    # if light level is >= 52
+    if light_level >= 52:
+        np[0] = (0, 0, 0)
+        np[1] = (0, 0, 0)
+        np[2] = (0, 0, 0)
+        np[3] = (0, 0, 0)
+        np.show()
+
+        # if light level is > 104
+    if light_level > 104:
+        np[0] = (0, 0, 0)
+        np[1] = (0, 0, 0)
+        np[2] = (0, 0, 0)
+        np[3] = (0, 0, 0)
+        np.show()
+    # if light level is > 156
+    if light_level > 156:
+        np[0] = (0, 0, 0)
+        np[1] = (0, 0, 0)
+        np[2] = (0, 0, 0)
+        np[3] = (0, 0, 0)
+        np.show()
+
+    # if light level is > 208
+    if light_level > 208:
+        np[0] = (0, 0, 0)
+        np[1] = (0, 0, 0)
+        np[2] = (0, 0, 0)
+        np[3] = (0, 0, 0)
+        np.show()
